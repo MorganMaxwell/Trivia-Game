64cacdefcabbb47e15030652c03e306be0b16fd5
Morgan M <morgan.maxwell20@gmail.com>
2018-11-29 13:21:52 -0700

    start timer is ready

--- js/logic.js
+++ js/logic.js
@@ -5,16 +5,13 @@
 // create an object? yeah, object with the property being the question
 // and the values being the answers
 
-// I think the easiest way for random positions with answers is to 
-// make a random num gen between 0 and 3, and each question position will be
-// assigned a number from that to then take the answer and write it to the page
-
 // I WANT TO DISPLAY EACH QUESTION, AND THEN OVERWRITE THE TEXT WITH A TIMER.
 // THAT WILL BE THE EASIEST WAY TO CHANGE THE QUESTION EACH TIME
 // "select#foo option:checked"
 
 $(document).ready(function () {
-
+    
+//variables block
     var data = {
         question1: [
             "question1",
@@ -43,57 +40,102 @@ $(document).ready(function () {
             "test4",
             "test4",
             "test4"
+        ],
+        startData: [
+            "The question will go Here",
+            "Answer1",
+            "Answer2",
+            "Answer3",
+            "Answer4"
         ]
     };
-    var startCounter = 5;
-    $(".actualTimer").text(startCounter);
+    var Counter = 5;
+    var intervalId;
+    var timerOn = false;
+// before game starts... information?
+    $(".actualTimer").text(Counter);
 
-
+// questions block
     function setQuestion1() {
+        // clearing out the 'start timer' so it won't keep going
+        // past 0;
+        clearInterval(intervalId);
+        // printing the information from data to the page
         $(".question").text(data.question1[0]);
         $(".q1").text(data.question1[1]);
         $(".q2").text(data.question1[2]);
         $(".q3").text(data.question1[3]);
         $(".q4").text(data.question1[4]);
+        // start the timer to answer the questions
+        Counter = 10;
+        questionTimer();
     };
-
     function setQuestion2() {
         $(".question").text(data.question2[0]);
         $(".q1").text(data.question2[1]);
         $(".q2").text(data.question2[2]);
         $(".q3").text(data.question2[3]);
         $(".q4").text(data.question2[4]);
+        Counter = 10;
+        questionTimer();
     };
-
     function setQuestion3() {
         $(".question").text(data.question3[0]);
         $(".q1").text(data.question3[1]);
         $(".q2").text(data.question3[2]);
         $(".q3").text(data.question3[3]);
         $(".q4").text(data.question3[4]);
+        Counter = 10;
+        questionTimer();
     };
-
     function setQuestion4() {
         $(".question").text(data.question4[0]);
         $(".q1").text(data.question4[1]);
         $(".q2").text(data.question4[2]);
         $(".q3").text(data.question4[3]);
         $(".q4").text(data.question4[4]);
+        Counter = 10;
+        questionTimer();
     };
-
+    function resetPage() {
+        Counter = 5;
+        timerOn = false;
+        clearInterval(intervalId);
+        $(".actualTimer").text(Counter);
+        $(".question").text(data.startData[0]);
+        $(".q1").text(data.startData[1]);
+        $(".q2").text(data.startData[2]);
+        $(".q3").text(data.startData[3]);
+        $(".q4").text(data.startData[4]);
+    };
+// timer to start the game
     function startTimer() {
-        setInterval(timerCounter, 1000);
-        setTimeout(function () {
-            setQuestion1();
-        }, 5000);
+        if (!timerOn) {
+            // set timerCounter to run every 1 second
+            intervalId = setInterval(timerCounter, 1000);
+            setTimeout(function () {
+                setQuestion1();
+            }, 5000);
+            timerOn = true;
+        };
     };
-
+    // this is to update the timer on the page
     function timerCounter() {
-        startCounter--;
-        $(".actualTimer").text(startCounter);
+        Counter--;
+        $(".actualTimer").text(Counter);
+        // if (Counter === 0) {
+        //     $(".actualTimer").text(10);
+        // };
     };
 
+// question timer
+    function questionTimer() {
+        intervalId = setInterval(timerCounter, 1000);
+    }
+
+// click events
     $("#startButton").on("click", startTimer);
+    $("#resetButton").on("click", resetPage);
 
 
 });
