cb8ceea71f03a9f51c95bbfce68fbb9856665b83
Morgan M <morgan.maxwell20@gmail.com>
2018-12-01 16:37:21 -0700

    endGame() might not work, array of functions is calling each function

--- js/logic.js
+++ js/logic.js
@@ -9,9 +9,18 @@
 // THAT WILL BE THE EASIEST WAY TO CHANGE THE QUESTION EACH TIME
 // "select#foo option:checked"
 
+// I want to have a timerCounter that loops. so maybe the best idea is to make a 
+//timerCounter that counts from 10 to 0, then updates a variable with a number 
+// stored in it. so var questionCount starts at 1, then the timer runs through
+// once, and it updates to 2. then make a long if else statement running a diff
+// function for whatever the value of questionCount is.
+// make an array with all the answers, and compare the user guess to
+// it at each point. 
+
+
 $(document).ready(function () {
-    
-//variables block
+
+    //variables block
     var data = {
         question1: [
             "question1",
@@ -41,34 +50,31 @@ $(document).ready(function () {
             "test4",
             "test4"
         ],
-        startData: [
-            "The question will go Here",
-            "Answer1",
-            "Answer2",
-            "Answer3",
-            "Answer4"
-        ]
+        // startData: [
+        //     "Get ready! You will have 10 seconds to answer each question",
+        //     "Answer1",
+        //     "Answer2",
+        //     "Answer3",
+        //     "Answer4"
+        // ]
     };
-    var Counter = 5;
+    var timerCounter = 10;
     var intervalId;
     var timerOn = false;
-// before game starts... information?
-    $(".actualTimer").text(Counter);
+    var userGuesses = [];
+    var correctAnswers = [1, 3, 1, 4];
+    var correctGuesses = 0;
+    var wrongGuesses = 0
 
-// questions block
+
+    // questionPicker block
     function setQuestion1() {
-        // clearing out the 'start timer' so it won't keep going
-        // past 0;
-        clearInterval(intervalId);
         // printing the information from data to the page
         $(".question").text(data.question1[0]);
         $(".q1").text(data.question1[1]);
         $(".q2").text(data.question1[2]);
         $(".q3").text(data.question1[3]);
         $(".q4").text(data.question1[4]);
-        // start the timer to answer the questions
-        Counter = 10;
-        questionTimer();
     };
     function setQuestion2() {
         $(".question").text(data.question2[0]);
@@ -76,8 +82,6 @@ $(document).ready(function () {
         $(".q2").text(data.question2[2]);
         $(".q3").text(data.question2[3]);
         $(".q4").text(data.question2[4]);
-        Counter = 10;
-        questionTimer();
     };
     function setQuestion3() {
         $(".question").text(data.question3[0]);
@@ -85,8 +89,6 @@ $(document).ready(function () {
         $(".q2").text(data.question3[2]);
         $(".q3").text(data.question3[3]);
         $(".q4").text(data.question3[4]);
-        Counter = 10;
-        questionTimer();
     };
     function setQuestion4() {
         $(".question").text(data.question4[0]);
@@ -94,47 +96,61 @@ $(document).ready(function () {
         $(".q2").text(data.question4[2]);
         $(".q3").text(data.question4[3]);
         $(".q4").text(data.question4[4]);
-        Counter = 10;
-        questionTimer();
     };
     function resetPage() {
-        Counter = 5;
+        timerCounter = 10;
         timerOn = false;
         clearInterval(intervalId);
-        $(".actualTimer").text(Counter);
+        $(".actualTimer").text(timerCounter);
         $(".question").text(data.startData[0]);
         $(".q1").text(data.startData[1]);
         $(".q2").text(data.startData[2]);
         $(".q3").text(data.startData[3]);
         $(".q4").text(data.startData[4]);
     };
-// timer to start the game
-    function startTimer() {
-        if (!timerOn) {
-            // set timerCounter to run every 1 second
-            intervalId = setInterval(timerCounter, 1000);
-            setTimeout(function () {
-                setQuestion1();
-            }, 5000);
-            timerOn = true;
+    // I'm hoping this will work and compare the user guesses to correct answers
+    // but it will probably break if they click a radio button before the game starts
+
+    function endGame() {
+        for (var i = 0; i < userGuesses; i++) {
+            var j = 0;
+            if (userGuesses[i] == correctAnswers[j]) {
+                correctGuesses++;
+            }
+            else if (userGuesses[i] != correctAnswers[j]) {
+                wrongGuesses++;
+            };
+            j++;
         };
     };
-    // this is to update the timer on the page
-    function timerCounter() {
-        Counter--;
-        $(".actualTimer").text(Counter);
-        // if (Counter === 0) {
-        //     $(".actualTimer").text(10);
-        // };
-    };
-
-// question timer
-    function questionTimer() {
-        intervalId = setInterval(timerCounter, 1000);
+    //start the game
+    function startGame() {
+        var questionCount = [setQuestion1(), setQuestion2(), setQuestion3(), setQuestion4()];
+        // for each thing in questionCount, run a timer
+        $.each(questionCount, gameTimer());
+        endGame();
     }
-
-// click events
-    $("#startButton").on("click", startTimer);
+    // this is to run the timer for 10 seconds each question, and to gather user
+    // input at the end of those 10 seconds
+    function gameTimer() {
+        intervalId = setInterval(timerPrinter, 1000);
+        setTimeout(function () {
+            dataGrab();
+        }, 10000);
+    }
+    // this is to actually print the timer numbers to the screen
+    function timerPrinter() {
+        $("#actualTimer").text(timerCounter);
+        timerCounter--;
+    };
+    // this is to gather user data for each question
+    function dataGrab() {
+        console.log($('input[name=answers]:checked').val());
+        userGuesses.push($('input[name=answers]:checked').val());
+        console.log(userGuesses);
+    }
+    // click events
+    $("#startButton").on("click", startGame);
     $("#resetButton").on("click", resetPage);
 
 
